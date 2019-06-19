87d86cb14fa1e67b925b7350385a2eabbf86a375
mikaelacurrier <mikaela.currier@gmail.com>
2019-06-19 18:08:17 -0400

    Updates

---
+++ precache-manifest.e0334c0006a93336a6f8c4ffe0c868b8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "895bc7de59413bb8029ac83cac8c9e1b",
+    "url": "/front-end-project-week/static/media/icons-16.895bc7de.woff"
+  },
+  {
+    "revision": "10368e6eee68499a3ca7",
+    "url": "/front-end-project-week/static/css/main.6c81326e.chunk.css"
+  },
+  {
+    "revision": "aa8f3ca044cd237f87b69d0cb364887a",
+    "url": "/front-end-project-week/static/media/icons-20.aa8f3ca0.ttf"
+  },
+  {
+    "revision": "c68b897b064b3ef38c9b",
+    "url": "/front-end-project-week/static/js/1.c68b897b.chunk.js"
+  },
+  {
+    "revision": "9c13ef4359f0b536b667",
+    "url": "/front-end-project-week/static/js/runtime~main.9c13ef43.js"
+  },
+  {
+    "revision": "3326c360137c2d6a6909d0c4303e502a",
+    "url": "/front-end-project-week/static/media/icons-16.3326c360.eot"
+  },
+  {
+    "revision": "10368e6eee68499a3ca7",
+    "url": "/front-end-project-week/static/js/main.10368e6e.chunk.js"
+  },
+  {
+    "revision": "b6382c6d9ffca4a1c50f58a17fce15a9",
+    "url": "/front-end-project-week/static/media/icons-16.b6382c6d.ttf"
+  },
+  {
+    "revision": "e666d692a7dc07ea69886dfcec837c3c",
+    "url": "/front-end-project-week/static/media/icons-20.e666d692.eot"
+  },
+  {
+    "revision": "058e48cbbee6cdbfb2a2e6dab347f245",
+    "url": "/front-end-project-week/static/media/icons-20.058e48cb.woff"
+  },
+  {
+    "revision": "c68b897b064b3ef38c9b",
+    "url": "/front-end-project-week/static/css/1.d8961542.chunk.css"
+  },
+  {
+    "revision": "3af7837094332204e7d31fc3113317c2",
+    "url": "/front-end-project-week/index.html"
+  }
+];
